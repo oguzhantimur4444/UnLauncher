420b45e09864ab4ef8544e5185d69ac81d55a57a
oguzh <oguzh@DESKTOP-EN8JJOB>
2024-08-27 02:08:07 +0300

    Proje dosyası ekle.

---
+++ UnLauncher/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace UnLauncher
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BottomBar = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.progressBar = new Guna.UI2.WinForms.Guna2ProgressBar();
+            this.usernameBox = new Guna.UI2.WinForms.Guna2TextBox();
+            this.playButton = new Guna.UI2.WinForms.Guna2Button();
+            this.versionsBox = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.usernameMessageDialog = new Guna.UI2.WinForms.Guna2MessageDialog();
+            this.guna2Panel1.SuspendLayout();
+            this.BottomBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.guna2Panel1.Controls.Add(this.guna2CircleButton2);
+            this.guna2Panel1.Controls.Add(this.guna2CircleButton1);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(800, 27);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(4, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UnLauncher";
+            // 
+            // BottomBar
+            // 
+            this.BottomBar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BottomBar.Controls.Add(this.versionsBox);
+            this.BottomBar.Controls.Add(this.playButton);
+            this.BottomBar.Controls.Add(this.usernameBox);
+            this.BottomBar.Controls.Add(this.progressBar);
+            this.BottomBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBar.Location = new System.Drawing.Point(0, 394);
+            this.BottomBar.Name = "BottomBar";
+            this.BottomBar.Size = new System.Drawing.Size(800, 56);
+            this.BottomBar.TabIndex = 1;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2PictureBox1.Image = global::UnLauncher.Properties.Resources.bg;
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(800, 367);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.guna2PictureBox1.TabIndex = 2;
+            this.guna2PictureBox1.TabStop = false;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.progressBar.Location = new System.Drawing.Point(0, 0);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.ProgressColor = System.Drawing.Color.Purple;
+            this.progressBar.ProgressColor2 = System.Drawing.Color.DarkOrchid;
+            this.progressBar.ShowText = true;
+            this.progressBar.Size = new System.Drawing.Size(800, 10);
+            this.progressBar.TabIndex = 0;
+            this.progressBar.TextMode = Guna.UI2.WinForms.Enums.ProgressBarTextMode.Custom;
+            this.progressBar.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SystemDefault;
+            this.progressBar.Value = 100;
+            // 
+            // usernameBox
+            // 
+            this.usernameBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.usernameBox.DefaultText = "";
+            this.usernameBox.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.usernameBox.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.usernameBox.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameBox.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.usernameBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usernameBox.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.usernameBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.usernameBox.Location = new System.Drawing.Point(0, 14);
+            this.usernameBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.usernameBox.Name = "usernameBox";
+            this.usernameBox.PasswordChar = '\0';
+            this.usernameBox.PlaceholderText = "";
+            this.usernameBox.SelectedText = "";
+            this.usernameBox.Size = new System.Drawing.Size(217, 42);
+            this.usernameBox.TabIndex = 1;
+            // 
+            // playButton
+            // 
+            this.playButton.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.playButton.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.playButton.DisabledState.FillColor = System.Drawing.Color.Purple;
+            this.playButton.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.playButton.FillColor = System.Drawing.Color.DarkOrchid;
+            this.playButton.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.playButton.ForeColor = System.Drawing.Color.White;
+            this.playButton.Location = new System.Drawing.Point(610, 11);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(190, 45);
+            this.playButton.TabIndex = 2;
+            this.playButton.Text = "Oyna";
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // versionsBox
+            // 
+            this.versionsBox.BackColor = System.Drawing.Color.Transparent;
+            this.versionsBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.versionsBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.versionsBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.versionsBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.versionsBox.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.versionsBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.versionsBox.ItemHeight = 30;
+            this.versionsBox.Location = new System.Drawing.Point(305, 14);
+            this.versionsBox.Name = "versionsBox";
+            this.versionsBox.Size = new System.Drawing.Size(190, 36);
+            this.versionsBox.TabIndex = 3;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 1D;
+            this.guna2DragControl1.DragStartTransparencyValue = 1D;
+            this.guna2DragControl1.TargetControl = this.guna2Panel1;
+            this.guna2DragControl1.TransparentWhileDrag = false;
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.Red;
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(776, 3);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(21, 21);
+            this.guna2CircleButton1.TabIndex = 1;
+            this.guna2CircleButton1.Text = "guna2CircleButton1";
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            // 
+            // guna2CircleButton2
+            // 
+            this.guna2CircleButton2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2CircleButton2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2CircleButton2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2CircleButton2.FillColor = System.Drawing.Color.Yellow;
+            this.guna2CircleButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton2.Location = new System.Drawing.Point(749, 3);
+            this.guna2CircleButton2.Name = "guna2CircleButton2";
+            this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton2.Size = new System.Drawing.Size(21, 21);
+            this.guna2CircleButton2.TabIndex = 2;
+            this.guna2CircleButton2.Text = "guna2CircleButton2";
+            this.guna2CircleButton2.Click += new System.EventHandler(this.guna2CircleButton2_Click);
+            // 
+            // usernameMessageDialog
+            // 
+            this.usernameMessageDialog.Buttons = Guna.UI2.WinForms.MessageDialogButtons.OK;
+            this.usernameMessageDialog.Caption = null;
+            this.usernameMessageDialog.Icon = Guna.UI2.WinForms.MessageDialogIcon.Warning;
+            this.usernameMessageDialog.Parent = this;
+            this.usernameMessageDialog.Style = Guna.UI2.WinForms.MessageDialogStyle.Default;
+            this.usernameMessageDialog.Text = "Lütfen bir kullanıcı adı giriniz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.guna2PictureBox1);
+            this.Controls.Add(this.BottomBar);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.BottomBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2Panel BottomBar;
+        private Guna.UI2.WinForms.Guna2ProgressBar progressBar;
+        private Guna.UI2.WinForms.Guna2ComboBox versionsBox;
+        private Guna.UI2.WinForms.Guna2Button playButton;
+        private Guna.UI2.WinForms.Guna2TextBox usernameBox;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
+        private Guna.UI2.WinForms.Guna2MessageDialog usernameMessageDialog;
+    }
+}
+
---
+++ UnLauncher/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CmlLib.Core;
+using CmlLib.Core.ProcessBuilder;
+using CmlLib.Core.Installers;
+using CmlLib.Core.Auth;
+
+namespace UnLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getVersions();
+        }
+
+        private async void getVersions()
+        {
+            var launcher = new MinecraftLauncher();
+            var versions = await launcher.GetAllVersionsAsync();
+            foreach (var version in versions)
+            {
+                if (version.Type=="release")
+                {
+                    versionsBox.Items.Add(version.Name);
+                }
+            }
+            versionsBox.SelectedIndex = 0;
+        }
+
+        private void guna2CircleButton1_Click(object sender, EventArgs e){ Application.Exit(); }
+
+        private async void playButton_Click(object sender, EventArgs e)
+        {
+            if (usernameBox.Text!="")
+            {
+                disableBottomBar();
+                var launcher = new MinecraftLauncher();
+                launcher.FileProgressChanged += progressChanged;
+                var process = await launcher.InstallAndBuildProcessAsync(versionsBox.SelectedItem.ToString(), new MLaunchOption
+                {
+                    Session = MSession.CreateOfflineSession(usernameBox.Text)
+                });
+                process.EnableRaisingEvents = true;
+                process.Exited += Process_Exited;
+                process.Start();
+                Hide();
+            }
+            else
+            {
+                usernameMessageDialog.Show();
+            }
+        }
+
+        private void disableBottomBar()
+        {
+            playButton.Enabled = false;
+            versionsBox.Enabled = false;
+            usernameBox.Enabled = false;
+            playButton.Text = "Yükleniyor...";
+        }
+
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            if (InvokeRequired)
+            {
+                // Ana iş parçacığı üzerinde çalıştırmak için Invoke kullanın
+                Invoke(new Action(() =>
+                {
+                    Show();
+                    enableBottomBar();
+                }));
+            }
+            else
+            {
+                Show();
+                enableBottomBar();
+            }
+        }
+
+        private void enableBottomBar()
+        {
+            progressBar.Text = "";
+            playButton.Enabled = true;
+            versionsBox.Enabled = true;
+            usernameBox.Enabled = true;
+            playButton.Text = "Oyna";
+        }
+
+        private void progressChanged(object sender, InstallerProgressChangedEventArgs e)
+        {
+            progressBar.Value = e.ProgressedTasks * 100 / e.TotalTasks;
+            progressBar.Text = string.Format("{0} {1} - {2}/{3}", e.EventType, e.Name, e.ProgressedTasks, e.TotalTasks);
+        }
+
+        private void guna2CircleButton2_Click(object sender, EventArgs e) { WindowState = FormWindowState.Minimized; }
+    }
+}
